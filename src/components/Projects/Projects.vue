8e5b03ca73a1f5828417a412a33fd0255eec12bd
lujingjing <lujingjing@example.com>
2018-05-28 16:28:28 +0800

    ljj01

--- src/components/Projects/Projects.vue
+++ src/components/Projects/Projects.vue
@@ -1,41 +1,48 @@
 <template>
     <div id="projects">
+        <!-- <table class="tab_projects">
+            <thead>
+                <tr>
+                    <th class="domain th" name="domain">Domain</th>
+                    <th class="system  th" name="system">System</th>
+                    <th class="creator  th" name="creator">Creator</th>
+                    <th class="remark th" name="remark">Remark</th>
+                    <th class="operating th" name="operating">Operating</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr class="tbtr">
+                    <td>11111</td>
+                    <td>1111</td>
+                    <td>111</td>
+                    <td>1111</td>
+                    <td>1111</td>
+                </tr>
+
+            </tbody>
+        </table> -->
+
         <el-table
-        :data="tableData3"
+        :data="tableData"
         style="width: 100%"
-        height="250">
+        highlight-current-row
+        size:medium 
+        >
         <el-table-column
-        fixed
-        prop="date"
-        label="Domain"
-        width="150">
+            prop="date"
+            label="日期"
+            width="180">
         </el-table-column>
         <el-table-column
-        prop="name"
-        label="System"
-        width="120">
+            prop="name"
+            label="姓名"
+            width="180">
         </el-table-column>
         <el-table-column
-        prop="province"
-        label="Creator"
-        width="120">
+            prop="address"
+            label="地址">
         </el-table-column>
-        <el-table-column
-        prop="city"
-        label="Remark"
-        width="120">
-        </el-table-column>
-        <el-table-column
-        prop="address"
-        label="Operating"
-        width="300">
-        </el-table-column>
-        <el-table-column
-        prop="zip"
-        label="邮编"
-        width="120">
-        </el-table-column>
-    </el-table>
+        </el-table>
     </div>
 </template>
 
@@ -46,55 +53,22 @@
 
       data(){
           return {
-            tableData3: [{
-            date: '2016-05-03',
-            name: '王小虎',
-            province: '上海',
-            city: '普陀区',
-            address: '上海市普陀区金沙江路 1518 弄',
-            zip: 200333
+            tableData: [{
+                date: '2016-05-02',
+                name: '王小虎',
+                address: '上海市普陀区金沙江路 1518 弄'
             }, {
-            date: '2016-05-02',
-            name: '王小虎',
-            province: '上海',
-            city: '普陀区',
-            address: '上海市普陀区金沙江路 1518 弄',
-            zip: 200333
+                date: '2016-05-04',
+                name: '王小虎',
+                address: '上海市普陀区金沙江路 1517 弄'
             }, {
-            date: '2016-05-04',
-            name: '王小虎',
-            province: '上海',
-            city: '普陀区',
-            address: '上海市普陀区金沙江路 1518 弄',
-            zip: 200333
+                date: '2016-05-01',
+                name: '王小虎',
+                address: '上海市普陀区金沙江路 1519 弄'
             }, {
-            date: '2016-05-01',
-            name: '王小虎',
-            province: '上海',
-            city: '普陀区',
-            address: '上海市普陀区金沙江路 1518 弄',
-            zip: 200333
-            }, {
-            date: '2016-05-08',
-            name: '王小虎',
-            province: '上海',
-            city: '普陀区',
-            address: '上海市普陀区金沙江路 1518 弄',
-            zip: 200333
-            }, {
-            date: '2016-05-06',
-            name: '王小虎',
-            province: '上海',
-            city: '普陀区',
-            address: '上海市普陀区金沙江路 1518 弄',
-            zip: 200333
-            }, {
-            date: '2016-05-07',
-            name: '王小虎',
-            province: '上海',
-            city: '普陀区',
-            address: '上海市普陀区金沙江路 1518 弄',
-            zip: 200333
+                date: '2016-05-03',
+                name: '王小虎',
+                address: '上海市普陀区金沙江路 1516 弄'
             }]
           }
       },
@@ -117,19 +91,16 @@
     }
 </script>
 
-<style lang="scss" scoped>
+<style >
     #projects{
         width: 100%;
-        // min-height: 400px;
-        // height: auto !important;
-        // height: 400px;
         height: 100%;
         padding: 30px 35px;
+        
     }
-    table{
-        border: 0;
-        td{
-            border-bottom: 0;
-        }
+    
+     #projects .el-table td, .el-table th.is-leaf{
+        border-bottom: 0;
     }
+   
 </style>
