fe29185f0cccc454d0f7e22eff015557939fc4e4
lujingjing <lujingjing@example.com>
2018-05-29 13:49:34 +0800

    aserq

--- src/components/Projects/Projects.vue
+++ src/components/Projects/Projects.vue
@@ -1,6 +1,5 @@
 <template>
     <div id="projects">
-        <i class="iconfont  icon-detail" value="132"></i><i class="iconfont icon-more"></i>
         <!-- <table class="tab_projects">
             <thead>
                 <tr>
@@ -19,7 +18,6 @@
                     <td>1111</td>
                     <td>1111</td>
                 </tr>
-
             </tbody>
         </table> -->
 
@@ -32,31 +30,31 @@
         <el-table-column
             prop="date"
             label="Domain"
-            width="180">
+            width="220">
         </el-table-column>
         <el-table-column
             prop="name"
             label="System"
-            width="180">
+            width="220">
         </el-table-column>
         <el-table-column
             prop="address"
-            label="Creator">
+            label="Creator"
+            width="220">
         </el-table-column>
         <el-table-column
             prop="name"
-            label="Remark"
-            width="180">
+            label="Remark">
         </el-table-column>
         <el-table-column
             prop="take"
             label="Operating" 
-            v-html="take">
+            v-html="take"
+            width="180">
            <template slot-scope="scope">
                 <i class="el-icon-edit-outline"></i>
                 <i class="el-icon-tickets"></i>
-                <span>123</span>
-                <i class="iconfont icon-detail" style="height:20px;width:20px;font-size:10px;"></i>
+                <i class="iconfont icon-detail"></i>
             </template>
         </el-table-column>
         </el-table>
@@ -75,22 +73,22 @@
                 date: '2016-05-02',
                 name: '王小虎',
                 address: '上海市普陀区金沙江路 1518 弄',
-                // take:`<i class="iconfont  icon-detail taskEntrance" value="132"></i><i class="iconfont icon-more active"></i>`
+                take:'<i class="iconfont  icon-detail taskEntrance" value="132"></i><i class="iconfont icon-more active"></i>'
             }, {
                 date: '2016-05-04',
                 name: '王小虎',
                 address: '上海市普陀区金沙江路 1517 弄',
-                // take:'<i class="iconfont  icon-detail taskEntrance" value="132"></i><i class="iconfont icon-more active"></i>'
+                take:'<i class="iconfont  icon-detail taskEntrance" value="132"></i><i class="iconfont icon-more active"></i>'
             }, {
                 date: '2016-05-01',
                 name: '王小虎',
                 address: '上海市普陀区金沙江路 1519 弄',
-                // take:'<i class="iconfont  icon-detail taskEntrance" value="132"></i><i class="iconfont icon-more active"></i>'
+                take:'<i class="iconfont  icon-detail taskEntrance" value="132"></i><i class="iconfont icon-more active"></i>'
             }, {
                 date: '2016-05-03',
                 name: '王小虎',
                 address: '上海市普陀区金沙江路 1516 弄',
-                // take:'<i class="iconfont  icon-detail taskEntrance" value="132"></i><i class="iconfont icon-more active"></i>'
+                take:'<i class="iconfont  icon-detail taskEntrance" value="132"></i><i class="iconfont icon-more active"></i>'
             }]
           }
       },
@@ -118,28 +116,45 @@
         width: 100%;
         height: 100%;
         padding: 30px 35px;
-        
     }
-    
-     #projects .el-table td{
+    #projects .el-table td{
         border-bottom: 0;
         height: 55px;
-        border-bottom: 10px solid #e9eef3;
+        border-bottom: 10px solid #f7f7f7;
     }
     #projects .el-table tr{
         height: 55px;
     }
     #projects  .el-table th.is-leaf{
-        background: #e9eef3;
-        line-height: 70px;
+        background: #f7f7f7;
     }
     #projects .has-gutter{
          height: 50px;
-        background: #e9eef3;
+        background: #f7f7f7;
 
     }
     #projects .el-table .cell{
-        padding: 10px;
+        /* padding: 4px; */
+        height: 31px;
+        line-height: 31px;
+    }
+    #projects table i:hover {
+        background-color: #fff;
+    }
+    #projects table i {
+        /* display: inline-block; */
+        width: 28px;
+        height: 28px;
+        font-size: 20px;
+        color: #666;
+        text-align: center;
+        line-height: 28px;
+        font-size: 12px;
+        transition: all 0.5s;
+        border-radius: 50%;
+        margin-right: 10px;
+        transition: all 0.5s;
+        line-height: 31px;
     }
     
    
