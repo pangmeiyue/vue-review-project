b36c2a607836a1fdb12fe50ba9a85f22f076f8f3
pang <1205757122@qq.com>
2018-06-05 10:48:08 +0800

    compliance添加

--- src/components/Environment/children/Compliance/Compliance.vue
+++ src/components/Environment/children/Compliance/Compliance.vue
@@ -3,36 +3,45 @@
     <div class="compliance_form">
       <h3>Create Compliance Review Tool</h3>
       <div class="compliance_from_cont">
-        <el-form class="form" ref="form" :model="form" label-width="100px">
-          <el-form-item label="Name">
-            <el-select v-model="form.name" placeholder="请选择活动区域">
-              <el-option label="option1" value="option1"></el-option>
-              <el-option label="option2" value="option2"></el-option>
+        <el-form class="form" ref="validateForm" :model="validateForm" label-width="100px">
+          <el-form-item label="Name" prop="comp_name" :rules="[
+          { required: true, message: '不能为空'}
+        ]">
+            <el-select v-model="validateForm.comp_name" placeholder="请选择活动区域">
+              <el-option :key="key" v-for="(item,key) in addNames" :label="item.name" :value="item.name"></el-option>
             </el-select>
           </el-form-item>
-          <el-form-item label="Port">
-            <el-input v-model="form.port"></el-input>
+          <el-form-item label="Port" prop="comp_port" :rules="[
+          { required: true, message: 'Please Input Port'}
+        ]">
+            <el-input v-model="validateForm.comp_port"></el-input>
           </el-form-item>
-          <el-form-item label="User Name">
-            <el-input v-model="form.username"></el-input>
+          <el-form-item label="User Name" prop="comp_username" :rules="[
+          { required: true, message: 'Please Input User Name'}
+        ]">
+            <el-input v-model="validateForm.comp_username"></el-input>
           </el-form-item>
-          <el-form-item label="Password">
-            <el-input v-model="form.password"></el-input>
+          <el-form-item label="Password" prop="comp_pwd" :rules="[
+          { required: true, message: 'Please Input Password'}
+        ]">
+            <el-input v-model="validateForm.comp_pwd"></el-input>
           </el-form-item>
-           <el-form-item label="Address">
-            <el-input v-model="form.address"></el-input>
+          <el-form-item label="Address" prop="comp_host" :rules="[
+          { required: true, message: 'Please Input Address'}
+        ]">
+            <el-input v-model="validateForm.comp_host"></el-input>
           </el-form-item>
           <el-form-item label="Version">
-            <el-input v-model="form.version"></el-input>
+            <el-input v-model="validateForm.comp_version"></el-input>
           </el-form-item>
           <el-form-item class="default" label="Default">
-            <el-switch v-model="form.default" active-color="#00653d"></el-switch>
+            <el-switch v-model="validateForm.is_default" active-color="#00653d"></el-switch>
           </el-form-item>
           <el-form-item class="status" label="Status">
-            <el-switch v-model="form.status" active-color="#00653d"></el-switch>
+            <el-switch v-model="validateForm.status" active-color="#00653d"></el-switch>
           </el-form-item>
           <el-form-item class="button-content">
-            <el-button  @click="onSubmit">Confirm</el-button>
+            <el-button @click="onSubmit('validateForm')">Confirm</el-button>
             <el-button>Cancel</el-button>
           </el-form-item>
         </el-form>
@@ -49,22 +58,20 @@
 
     data() {
       return {
-        form: {
-          name: '',
-          port: '',
-          address: '',
-          username: '',
-          password: '',
-          version: '',
-          default: false,
+        addNames: [], //Names
+        validateForm: {
+          comp_name: 'SONAR',
+          comp_port: '',
+          comp_host: '',
+          comp_username: '',
+          comp_pwd: '',
+          comp_version: '',
+          is_default: false,
           status: true,
-          region: '',
-          date1: '',
-          date2: '',
-          delivery: false,
-          type: [],
-          resource: '',
-          desc: ''
+          sysDict: {
+            type_id: '12'
+          },
+          sys_comp_tool_id: ''
         }
       }
     },
@@ -86,6 +93,8 @@
         let scroll = document.getElementsByClassName('el-main main')[0].scrollTop
         document.getElementsByClassName('el-main main')[0].scrollTop = 0
       })
+      //获取Name
+      this.getNames()
     },
 
     mounted: function () {
@@ -93,8 +102,47 @@
     },
 
     methods: {
-      onSubmit() {
-        console.log('submit!');
+      onSubmit(formName) {
+        this.validateForm.is_default = this.validateForm.is_default ? 1 : 0;
+        this.validateForm.status = this.validateForm.status ? 1 : 0
+        console.log(this.validateForm)
+        this.$refs[formName].validate(valid => {
+          if (valid) {
+            this.$axios({
+              method: 'POST',
+              url: 'api/ciapp-server/systool/compliance_merge',
+              data: this.validateForm
+            }).then(res => {
+              if(res.data.success == false){
+                this.$message({
+                  message: res.data.message,
+                  iconClass: 'icon',
+                  center : true
+                });
+              }
+            })
+          } else {
+            return
+          }
+        })
+      },
+      getNames() {
+        this.$axios({
+          method: 'POST',
+          url: 'api/ciapp-server/dict/typeTool_3',
+        }).then(reponse => {
+          console.log(reponse)
+          this.addNames = reponse.data
+          this.typeId = reponse
+        })
+      },
+      //提示框
+      openMessageBox(message, type) {
+        console.log('type', type)
+        this.$alert(message, "提示", {
+          confirmButtonText: "确定",
+          type: type
+        });
       }
 
     }
@@ -134,8 +182,8 @@
           display: inline-block;
           width: 49%;
         }
-        .button-content{
-            text-align: center;
+        .button-content {
+          text-align: center;
         }
       }
       .code_from_cont {
